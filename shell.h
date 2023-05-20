d3b25213d0fcb0fda4ff0a9c1c66592a8ac8e698
Joseph Abang <abangjosef@gmail.com>
2023-05-20 03:36:55 -0700

    add updated header files

---
+++ shell.h
@@ -0,0 +1,46 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_ARGS 10
+#define MAX_PATHS 10
+#define PATH_MAX_LENGTH 100
+#define READ_BUF_SIZE 1024
+
+extern char **environ;
+
+/**
+ * struct shell_data - Shell data structure
+ * @paths: Array of paths
+ * @line: Pointer to the input line
+ * @args: Array of arguments
+ * @num_args: Number of arguments
+ */
+
+typedef struct shell_data
+{
+	char *paths[MAX_PATHS];
+	char *line;
+	char *args[MAX_ARGS];
+	int num_args;
+} shell_data;
+
+char *get_path(struct shell_data *data, char *command);
+void tokenize(struct shell_data *data);
+void print_env(void);
+void free_shell_data(shell_data *data);
+void execute_command(shell_data *data);
+int read_shell_input(shell_data *data);
+void init_shell_data(shell_data *data);
+ssize_t _getline(shell_data *data);
+void run_shell_command(shell_data *data);
+
+
+#endif /* SHELL_H */
+
