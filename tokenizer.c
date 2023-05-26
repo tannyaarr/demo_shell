4ea7a0d701f5666999416867614d1379421e9777
Joseph Abang <abangjosef@gmail.com>
2023-05-26 10:44:35 -0700

    Handled comment (#)

--- tokenizer.c
+++ tokenizer.c
@@ -36,28 +36,32 @@ char *_strpbrk(const char *str, const char *accept)
  * @data: shell data struct
  * Return: void
  */
+
+
 void tokenize(shell_data *data)
 {
-	const char *delim = " \t\n";
-	char *token;
-	char *line = data->line;
-	int num_args = 0;
+    const char *delim = " \t\n";
+    char *line = data->line;
+    int num_args = 0;
 
-	token = _strpbrk(line, delim);
-	while (token != NULL && num_args < MAX_ARGS - 1)
-	{
-		*token = '\0';
-		data->args[num_args++] = line;
-		line = token + 1;
-		token = _strpbrk(line, delim);
-	}
+    while (*line && num_args < MAX_ARGS - 1)
+    {
+        while (*line && strchr(delim, *line))
+            line++;
 
-	if (line[0] != '\0' && num_args < MAX_ARGS - 1)
-	{
-		data->args[num_args++] = line;
-	}
+        if (*line == '\0' || *line == '#')
+            break;
 
-	data->args[num_args] = NULL;
-	data->num_args = num_args;
+        data->args[num_args++] = line;
+
+        while (*line && !strchr(delim, *line))
+            line++;
+
+        if (*line)
+            *line++ = '\0';
+    }
+
+    data->args[num_args] = NULL;
+    data->num_args = num_args;
 }
 
